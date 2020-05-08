8638024cd3e8e5bd84918ee1854b29d61dcebd4f
Corbett Bennett <corbettb@alleninstitute.org>
2020-05-08 11:13:46 -0700

    scroll bar and sync data

--- lickVideo.py
+++ lickVideo.py
@@ -18,6 +18,8 @@ import pyqtgraph as pg
 import cv2
 import os
 from datetime import datetime
+from sync_dataset import Dataset
+
 
 def start():
     #QtGui.QApplication.setGraphicsSystem("raster")
@@ -36,23 +38,37 @@ class lickVideo():
         self.annotationDataFile = None
         self.lastAnnotatedFrame = None
         self.videoFileName = None
+        self.sync_lick_frames = []
         self.frameIndex = 0
         self.mainWin = QtGui.QMainWindow()
         self.mainWidget = QtGui.QWidget()
         self.mainWin.setCentralWidget(self.mainWidget)
         self.mainWin.closeEvent = self.closeEvent
         self.mainLayout = QtGui.QGridLayout()
-        self.mainWidget.setLayout(self.mainLayout)
+        
+        
         self.createMenuBar()
         self.createControlPanel()
         
         self.mainWin.keyPressEvent = self.keyPressCallback
+        
+        self.plotLayout = pg.GraphicsLayoutWidget()
+        self.plot1 = self.plotLayout.addPlot(0,0)
+        self.plot1.setLimits(minYRange=2)
+        self.plot1_infLine = pg.InfiniteLine(movable=True)
+        self.plot1_infLine.sigDragged.connect(self.scrollFrame)
+        self.plot1.addItem(self.plot1_infLine)
+
+        self.mainLayout.addWidget(self.plotLayout, 2, 0)
+        
         self.imageLayout = pg.GraphicsLayoutWidget()
         self.imageViewBox = self.imageLayout.addViewBox(lockAspect=1,invertY=True,enableMouse=True,enableMenu=True)
         self.imageItem = pg.ImageItem()
         self.imageViewBox.addItem(self.imageItem)   
+        self.mainLayout.addWidget(self.imageLayout, 1, 0)
+        self.mainLayout.setRowMinimumHeight(1, 500)
+        self.mainWidget.setLayout(self.mainLayout)
         
-        self.mainLayout.addWidget(self.imageLayout)
         self.mainWin.show()
 
         self.annotationDataSaved = False
@@ -75,6 +91,10 @@ class lickVideo():
         self.updatePlot()
 
         self.totalVidFrames = self.vid.get(cv2.CAP_PROP_FRAME_COUNT)
+        
+        self.plot1.setXRange(0, 18000, padding=0)
+        self.plot1.setLimits(xMin=0, xMax=self.totalVidFrames, yMin=0, yMax=2)
+        
         print(self.totalVidFrames)
         self.frameRate = self.vid.get(cv2.CAP_PROP_FPS)
         self.frameIndex = 0
@@ -113,6 +133,24 @@ class lickVideo():
             assert(len(self.lickStates)==self.totalVidFrames)
             self.frameIndex = self.lastAnnotatedFrame
             self.updatePlot()
+            
+    def loadSyncFile(self):
+        self.resetPlot('syncDataItems')
+        syncFile = QtGui.QFileDialog.getOpenFileName(self.mainWin, 'Load Sync Data', filter='*.h5; *.sync')
+        if isinstance(syncFile, tuple):
+            syncFile = str(syncFile[0])
+        self.syncFile = str(syncFile)
+
+        syncDataset = Dataset(self.syncFile)
+        sync_frame_times, _ = get_sync_line_data(syncDataset, 'cam1_exposure')
+        sync_lick_times, _ = get_sync_line_data(syncDataset, channel=31)
+        
+        print(len(sync_frame_times))
+        print(len(sync_lick_times))
+        
+        self.sync_lick_frames = np.searchsorted(sync_frame_times, sync_lick_times) - 1
+        
+        self.syncDataItems = self.plot1.plot(self.sync_lick_frames, np.ones(len(self.sync_lick_frames)), size=0.5, pen=None, symbol='t')
     
     def saveAnnotationData(self, automaticName=False):
         now = datetime.now()
@@ -152,7 +190,12 @@ class lickVideo():
         self.lickStates = np.zeros(int(self.totalVidFrames))
         self.lastAnnotatedFrame = None
         self.setRadioButtonStates()
-        
+    
+    def resetPlot(self, plotDataItemName):
+        if hasattr(self, plotDataItemName):
+            for pdi in self[plotDataItemName]:
+                pdi.clear()   
+    
     def createMenuBar(self):
         # create an instance of menu bar
         menubar = self.mainWin.menuBar()
@@ -169,9 +212,13 @@ class lickVideo():
         loadAnnotations_action = QtGui.QAction('&Load Annotation Data', self.mainWin)
         loadAnnotations_action.triggered.connect(self.loadAnnotationData)
     
+        loadSync_action = QtGui.QAction('&Load Sync Data', self.mainWin)
+        loadSync_action.triggered.connect(self.loadSyncFile)
+        
         file_menu.addAction(open_action)
         file_menu.addAction(loadAnnotations_action)
         file_menu.addAction(saveAnnotations_action)
+        file_menu.addAction(loadSync_action)
              
     def createControlPanel(self):
         #make layout for gui controls and add to main layout
@@ -221,18 +268,36 @@ class lickVideo():
         self.noLickRadioButton.setToolTip('Shortcut: N')
         self.controlPanelLayout.addWidget(self.noLickRadioButton, 0, 8, 1, 1)
 
-    def advanceFrame(self):
+    def advanceFrame(self, toNextDetectorFrame=False):
+        
         self.frameIndex += 1
         if self.frameIndex > self.totalVidFrames:
             self.frameIndex = self.totalVidFrames
         
+        if toNextDetectorFrame:
+            nextDetectorFrameIndex = np.searchsorted(self.sync_lick_frames, self.frameIndex)
+            nextDetectorFrameIndex = np.min([nextDetectorFrameIndex, len(self.sync_lick_frames)-1])
+            self.frameIndex = self.sync_lick_frames[nextDetectorFrameIndex]
+            
         self.updatePlot()
         
-    def backFrame(self):
-        self.frameIndex -= 1
-        if self.frameIndex < 0:
-            self.frameIndex = 0
+    def backFrame(self, toLastDetectorFrame=False):
         
+        if toLastDetectorFrame:
+            lastDetectorFrameIndex = np.searchsorted(self.sync_lick_frames, self.frameIndex) - 1
+            lastDetectorFrameIndex = np.max([lastDetectorFrameIndex, 0])
+            self.frameIndex = self.sync_lick_frames[lastDetectorFrameIndex]
+        
+        else:
+            self.frameIndex -= 1
+            if self.frameIndex < 0:
+                self.frameIndex = 0
+            
+        self.updatePlot()
+    
+    def scrollFrame(self):
+        linePos = int(np.round(self.plot1_infLine.value()))
+        self.frameIndex = linePos
         self.updatePlot()
 
     def goToFrame(self):
@@ -262,8 +327,20 @@ class lickVideo():
             self.imageItem.setImage(self.frame[:,:,0].T)
             self.frameDisplayBox.setText(str(self.frameIndex))
             self.setRadioButtonStates()
+            self.updateLine()
         
-        
+    def updateLine(self):
+        self.plot1_infLine.setValue(self.frameIndex)
+        if self.frameIndex in self.sync_lick_frames:
+            self.plot1_infLine.setPen('r', width=5)
+        else:
+            self.plot1_infLine.setPen('y')
+        xMin, xMax = self.plot1.viewRange()[0]
+        plotrange = xMax-xMin
+        if self.frameIndex>0.9*xMax:
+            self.plot1.setXRange(self.frameIndex+0.1*plotrange, self.frameIndex - 0.9*plotrange, padding=0)
+
+    
     def setRadioButtonStates(self):
         if self.lickStates is not None:
             thisState = self.lickStates[self.frameIndex]
@@ -307,8 +384,41 @@ class lickVideo():
             self.missRadioButton.click()
         if event.key() == QtCore.Qt.Key_Space:
             self.playVideoButton.click()
+        if event.key() == QtCore.Qt.Key_Comma:
+            self.backFrame(toLastDetectorFrame=True)
+        if event.key() == QtCore.Qt.Key_Period:
+            self.advanceFrame(toNextDetectorFrame=True)
             
+            
+def get_sync_line_data(syncDataset, line_label=None, channel=None):
+    ''' Get rising and falling edge times for a particular line from the sync h5 file
+        
+        Parameters
+        ----------
+        dataset: sync file dataset generated by sync.Dataset
+        line_label: string specifying which line to read, if that line was labelled during acquisition
+        channel: integer specifying which channel to read if line wasn't labelled
+        
+        Returns
+        ----------
+        rising: npy array with rising edge times for specified line
+        falling: falling edge times
+    '''
     
+    if line_label in syncDataset.line_labels:
+        channel = syncDataset.line_labels.index(line_label)
+    elif channel is None:
+        print('Invalid Line Label: ' + line_label)
+        return
+    else:
+        print('No line label specified, reading channel ' + str(channel))
+    
+    sample_freq = syncDataset.meta_data['ni_daq']['counter_output_freq']
+    rising = syncDataset.get_rising_edges(channel)/sample_freq
+    falling = syncDataset.get_falling_edges(channel)/sample_freq
+    
+    return rising, falling
+   
         
 if __name__ == '__main__':
     start()
