1e33d62bf6441bef997f1cc1dff18e5bd40520b1
corbennett <corbennett@gmail.com>
2020-06-24 12:50:10 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
-conda create --name lick_annotation python==3.7 --yes
-pip install opencv-python
-pip install pyqtgraph
-pip install h5py
+call conda create --name lick_annotation python==3.7 --yes
+call conda activate lick_annotation
+call pip install opencv-python
+call pip install PyQt5
+call pip install pyqtgraph
+call pip install h5py
